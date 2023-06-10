8d54847cf876e10685bdbbe862d588ec26ed27ce
ccarsonclay <carsonaclay@gmail.com>
2023-06-10 00:20:41 -0400

    first commit

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ def greet():
 greet()
 
 
+def pack():
+  print('Dinner', 'lunch', 'breakfast')
+
+pack()
+
+lunch_items = ["tacos", "chips", "cereal"]
+
+def eat_lunch(food_list):
+    if not food_list:
+        print("My lunchbox is empty!")
+    else:
+        print("First I eat", food_list[0])
+        for food in food_list[1:]:
+            print("Next I eat", food)
+                  
+eat_lunch(lunch_items)
